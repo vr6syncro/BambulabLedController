a48cb4536b838a83424a995b384920adf205bd1f
vr6syncro <info@vr6-syncro.de>
2023-06-04 14:56:03 +0200

    switch to Neopixel
    initial switch to neopixel

---
+++ BambulabDirectConnectionLedstrip - Kopie/rm/led_utils.cpp
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+#include "led_utils.h"
+
+#define LED_PIN_R 13  // Red pin
+#define LED_PIN_G 12 // Green pin
+#define LED_PIN_B 14  // Blue pin
+#define LED_PIN_W 4  // White pin
+#define LED_PIN_WW 5 // Warm white pin
+
+int startR = 0;
+int startG = 0;
+int startB = 0;
+int startC = 0;
+int startW = 0;
+
+void setLedColor(int redValue, int greenValue, int blueValue, int coldValue, int warmValue) { //Function to change ledstrip color
+    if (redValue == startR && greenValue == startG && blueValue == startB && coldValue == startC && warmValue == startW) {
+        return;
+    }
+   transitionLedColor(redValue,greenValue,blueValue,coldValue,warmValue,100);
+}
+
+void pulseLedColor(int redValue, int greenValue, int blueValue, int coldValue, int warmValue) { //Function to change ledstrip color
+    if (redValue == startR && greenValue == startG && blueValue == startB && coldValue == startC && warmValue == startW) {
+        return;
+    }
+    int oldR = startR;
+    int oldG = startG;
+    int oldB = startB;
+    int oldC = startC;
+    int oldW = startW;
+    transitionLedColor(redValue,greenValue,blueValue,coldValue,warmValue,50);
+    transitionLedColor(oldR,oldG,oldB,oldC,oldW,50);
+}
+
+void setPins(int redValue, int greenValue, int blueValue, int coldValue, int warmValue) { //Function to change lestrip pins
+    pinMode(LED_PIN_R, OUTPUT);
+    pinMode(LED_PIN_G, OUTPUT);
+    pinMode(LED_PIN_B, OUTPUT);
+    pinMode(LED_PIN_W, OUTPUT);
+    pinMode(LED_PIN_WW, OUTPUT);
+
+    analogWrite(LED_PIN_R, redValue);
+    analogWrite(LED_PIN_G, greenValue);
+    analogWrite(LED_PIN_B, blueValue);
+    analogWrite(LED_PIN_W, coldValue);
+    analogWrite(LED_PIN_WW, warmValue);
+}
+
+void transitionLedColor(int endR, int endG, int endB, int endC, int endW, int duration) { //Function to tween color change
+    float stepTime = (float)duration / 255.0;
+    int rStep = (endR - startR) / 255;
+    int gStep = (endG - startG) / 255;
+    int bStep = (endB - startB) / 255;
+    int cStep = (endC - startC) / 255;
+    int wStep = (endW - startW) / 255;
+    
+    for (int i = 0; i < 256; i++) {
+        int r = startR + i * rStep;
+        int g = startG + i * gStep;
+        int b = startB + i * bStep;
+        int c = startC + i * cStep;
+        int w = startW + i * wStep;
+        
+        setPins(r, g, b, c, w);
+        delay(stepTime);
+    }
+    startR = endR;
+    startG = endG;
+    startB = endB;
+    startC = endC;
+    startW = endW;
+}
---
+++ BambulabDirectConnectionLedstrip/variables.h
@@ -0,0 +1,17 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#include <Arduino.h>
+
+const int Max_ipLength = 15;
+const int Max_accessCode = 8;
+const int Max_DeviceId = 15;
+const int Max_EspPassword = 8;
+
+const int Ip_Adress = 0;
+const int Accesscode_Adress = 15;
+const int DeviceId_Adress = 23;
+const int EspPassword_Adress = 38;
+
+
+#endif
